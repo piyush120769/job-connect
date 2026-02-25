50c056c4ca8295fcbce0699fde9f4f5627ab6b83
Piyush1207699 <mehtapiyush1207@gmail.com>
2026-02-26 00:11:21 +0530

    initial push

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
-});
+     baseURL: process.env.REACT_APP_API_URL || '/api',
+   });
 
 api.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('user') || 'null');
